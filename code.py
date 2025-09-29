c541665988b83f9c7ed83ff6a133b20023bde124
CreepSplotion <dekaiyondavis8@gmail.com>
2025-09-30 05:49:27 +1400

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,68 @@
+listOfECRAMembers = input('Do you want a list of all ECRA members ranked?(yes/no): ')
+
+if listOfECRAMembers = 'yes':
+  print('1. CreepSplotion')
+  print('2. Ennard')
+  print('3. ReapSplotion')
+  print('4. Division')
+  print('5. Giggles')
+  print('6. CreepYT')
+  print('7. Dash')
+  print('8. XxFun')
+
 userPlayer = input('What player would you like to see the stats of?: ')
 
 if (userPlayer == 'CreepSplotion'):
-  print('CreepSplotion's username is "ECRAYungyBot"')
-  creepPastUsernames = input('Do you want to see CreepSplotion's past usernames?(yes/no): ')
+  print("CreepSplotion's username is ECRAYungyBot")
+  print('CreepSplotion is rank #1 out of all ECRA members')
+  creepRankHistory = input("Do you want to see CreepSplotion's rank history?(yes/no): ")
+
+  if creepRankHistory == 'yes':
+    print('CreepSplotion has only been in the #1 placement')
+  creepPastUsernames = input("Do you want to see CreepSplotion's past usernames?(yes/no): ")
 
 if (creepPastUsernames == 'yes'):
+  print('ECRACreeperBot')
+  print('ECRAShadowCreep')
+  print('ECRACreep')
+  print('CreepSplotionYT')
+creepSkinMC = input("Do you want to see CreepSplotion's Skin MC account?(yes/no): ")
+if creepSkinMC == 'yes':
+  print('https://namemc.com/profile/ECRAYungyBot.1')
+
+if (userPlayer == 'ReapSplotion'):
+  print("ReapSplotion's username is ECRAReap")
+  print('ReapSplotion is rank #3 out of all ECRA members')
+  reapRankHistory = input("Do you want to see ReapSplotion's rank history?(yes/no): ")
+
+  if reapRankHistory == 'yes':
+    print('2018: #2')
+    print('2023: #3')
+    print('2024: #4')
+    print('2025: #3')
+  reapPastUsernames = input("Do you want to see ReapSplotion's past usernames?(yes/no): ")
+
+if (reapPastUsernames == 'yes'):
+  print('ReapSplotion has no past usernames')
+
+if (userPlayer == 'Ennard'):
+  print("Ennard's username is ECRAExoticEnnard")
+  print('Ennard is rank #2 out of all ECRA members')
+  ennardRankHistory = input("Do you want to see Ennard's rank history?(yes/no): ")
+
+  if ennardRankHistory == 'yes':
+    print('2018: #4')
+    print('2023: #3')
+    print('2024: #2')
+  ennardPastUsernames = input("Do you want to see Ennard's past usernames?(yes/no): ")
+
+if (ennardPastUsernames == 'yes'):
+  print('EqqqEnnard')
+  print('ExoticEnnardGG')
+  print('EXENNJJERD')
+  print('ECRAEnnard')
+  print('ECRAMrFeast')
+  print('ECRAEnnard')
+  print('ECRAExoticEnnard')
+  print('OffHandIsA')
+  print('IWalkWithQ')
