9eff752393e7003032c3b472e8c88d3d88789c77
CreepSplotion <dekaiyondavis8@gmail.com>
2025-09-30 10:29:20 +1400

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 listOfECRAMembers = input('Do you want a list of all ECRA members ranked?(yes/no): ')
 
-if listOfECRAMembers = 'yes':
+if listOfECRAMembers == 'yes':
   print('1. CreepSplotion')
   print('2. Ennard')
   print('3. ReapSplotion')
@@ -21,29 +21,29 @@ if (userPlayer == 'CreepSplotion'):
     print('CreepSplotion has only been in the #1 placement')
   creepPastUsernames = input("Do you want to see CreepSplotion's past usernames?(yes/no): ")
 
-if (creepPastUsernames == 'yes'):
-  print('ECRACreeperBot')
-  print('ECRAShadowCreep')
-  print('ECRACreep')
-  print('CreepSplotionYT')
-creepSkinMC = input("Do you want to see CreepSplotion's Skin MC account?(yes/no): ")
-if creepSkinMC == 'yes':
-  print('https://namemc.com/profile/ECRAYungyBot.1')
+  if (creepPastUsernames == 'yes'):
+    print('ECRACreeperBot')
+    print('ECRAShadowCreep')
+    print('ECRACreep')
+    print('CreepSplotionYT')
+  creepSkinMC = input("Do you want to see CreepSplotion's Skin MC account?(yes/no): ")
+  if creepSkinMC == 'yes':
+    print('https://namemc.com/profile/ECRAYungyBot.1')
 
 if (userPlayer == 'ReapSplotion'):
-  print("ReapSplotion's username is ECRAReap")
-  print('ReapSplotion is rank #3 out of all ECRA members')
-  reapRankHistory = input("Do you want to see ReapSplotion's rank history?(yes/no): ")
+    print("ReapSplotion's username is ECRAReap")
+    print('ReapSplotion is rank #3 out of all ECRA members')
+    reapRankHistory = input("Do you want to see ReapSplotion's rank history?(yes/no): ")
 
-  if reapRankHistory == 'yes':
-    print('2018: #2')
-    print('2023: #3')
-    print('2024: #4')
-    print('2025: #3')
-  reapPastUsernames = input("Do you want to see ReapSplotion's past usernames?(yes/no): ")
+    if reapRankHistory == 'yes':
+      print('2018: #2')
+      print('2023: #3')
+      print('2024: #4')
+      print('2025: #3')
+    reapPastUsernames = input("Do you want to see ReapSplotion's past usernames?(yes/no): ")
 
-if (reapPastUsernames == 'yes'):
-  print('ReapSplotion has no past usernames')
+    if (reapPastUsernames == 'yes'):
+      print('ReapSplotion has no past usernames')
 
 if (userPlayer == 'Ennard'):
   print("Ennard's username is ECRAExoticEnnard")
@@ -52,17 +52,82 @@ if (userPlayer == 'Ennard'):
 
   if ennardRankHistory == 'yes':
     print('2018: #4')
-    print('2023: #3')
     print('2024: #2')
   ennardPastUsernames = input("Do you want to see Ennard's past usernames?(yes/no): ")
 
-if (ennardPastUsernames == 'yes'):
-  print('EqqqEnnard')
-  print('ExoticEnnardGG')
-  print('EXENNJJERD')
-  print('ECRAEnnard')
-  print('ECRAMrFeast')
-  print('ECRAEnnard')
-  print('ECRAExoticEnnard')
-  print('OffHandIsA')
-  print('IWalkWithQ')
+  if (ennardPastUsernames == 'yes'):
+    print('EqqqEnnard')
+    print('ExoticEnnardGG')
+    print('EXENNJJERD')
+    print('ECRAEnnard')
+    print('ECRAMrFeast')
+    print('ECRAEnnard')
+    print('ECRAExoticEnnard')
+    print('OffHandIsA')
+    print('IWalkWithQ')
+
+if (userPlayer == 'Division'):
+  print("Division's username is ECRADividedByE")
+  print('Division is rank #4 out of all ECRA members')
+  divisionRankHistory = input("Do you want to see Division's rank history?(yes/no): ")
+
+  if divisionRankHistory == 'yes':
+    print('2018: #3')
+    print('2023: #2')
+    print('2024: #3')
+    print('2025: #4')
+  divisionPastUsernames = input("Do you want to see Division's past usernames?(yes/no): ")
+
+  if (divisionPastUsernames == 'yes'):
+    print('BetterCreep')
+    print('DivideBy78')
+    print('Division404')
+    print('ExoticDivision')
+    print('CSDivision')
+    print('ReaperD')
+    print('ECRANo')
+    print('ECRAyes')
+    print('ECRABestMember')
+    print('CreepSplotionYT')
+    print('ECRADivision')
+    print('12LengthTables')
+    print('ECRAFiftyFour')
+    print('HistoricalName')
+    print('ImportantFile')
+    print('GoodUsername')
+    print('ECRAFinally')
+    print('ECRADivision')
+    print('EnnardViolateCode')
+
+if (userPlayer == 'Giggles'):
+  print("Giggles' username is ECRAGiggles")
+  print('Giggles is rank #5 out of all ECRA members')
+  gigglesRankHistory = input("Do you want to see Giggles' rank history?(yes/no): ")
+
+  if gigglesRankHistory == 'yes':
+    print('Giggles joined in 2025')
+    print('2025: #5')
+  gigglesPastUsernames = input("Do you want to see Giggles' past usernames?(yes/no): ")
+
+  if (gigglesPastUsernames == 'yes'):
+    print("ExoticGiggles (He wasn't actually in EC)")
+    print('ECRAGiggles')
+    print('ECRANumber5')
+    print("ECRANumber3 (He was 3rd in ranking after a bet but lost it to more bets)") 
+    print('ECRANumber4')
+    print('ECRANumber5')
+    print('ExoticGiggles')
+
+if (userPlayer == 'CreepYT'):
+  print("CreepYT's username is CreepYT8451")
+  print('CreepYT is rank #6 out of all ECRA members')
+  print("From now on we'll refer to CreepYT as his real name which is Cillian")
+  cillianRankHistory = input("Do you want to see Cillian's rank history?(yes/no): ")
+
+  if cillianRankHistory == 'yes':
+    print('Giggles joined in 2025')
+    print('2025: #6')
+  cillianPastUsernames = input("Do you want to see Cillian's past usernames?(yes/no): ")
+
+  if (cillianPastUsernames == 'yes'):
+    print('Cillian has no past usernames')
