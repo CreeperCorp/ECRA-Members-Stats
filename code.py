52b6fb5eda336d607766b03a1b7866e8a0a9f4de
CreepSplotion <dekaiyondavis8@gmail.com>
2025-09-28 22:11:07 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,5 @@
+userPlayer = input('What player would you like to see the stats of?: ')
+
+if (userPlayer == 'CreepSplotion'):
+  print('CreepSplotion's username is "ECRAYungyBot"')
+  creepPastUsernames = input('Do you want to see CreepSplotion's past usernames
